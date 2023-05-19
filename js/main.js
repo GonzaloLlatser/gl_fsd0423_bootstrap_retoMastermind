da5b75e71953d87a55b11920a71dfc84b3ea6c2f
Gonza <gonl@gej.com>
2023-05-19 15:19:06 +0200

    fix: Corrige errores de código

--- js/main.js
+++ js/main.js
@@ -1,4 +1,10 @@
-const primeraOpcion = ()=> {
-    document.getElementById("row4first").style.display = "none";
-    document.getElementById("row4second").style.display = "none";
-}
+// const primeraOpcion = ()=> {
+//     document.getElementById("row4first").style.display = "none";
+//     document.getElementById("row4second").style.display = "none";
+// }
+
+
+let principiante = document.getElementById("pepe");
+principiante.addEventListener("click",()=>{
+    alert("Hola");
+});
