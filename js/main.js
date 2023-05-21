05b48614fb9cf5189880e71469d8d88477fd0749
Gonza <gonl@gej.com>
2023-05-21 20:45:51 +0200

    feat: Eliminar pagina colores y crear nuevas de seleccion de colores dependiendo el nivel. Se hace primera funcion de js donde coge y almacena el codigo del color para futuros usos de estos parametros

--- js/main.js
+++ js/main.js
@@ -5,7 +5,8 @@
 
 //funcion de nivel: inicial y 4 colores
 const haciaColores4 = ( ) => {
-    window.location.href="../pages/seleccioncolores.html";
+    window.location.href="../pages/selcolorniv1.html";
+ 
 }
 
 
@@ -13,7 +14,7 @@ const haciaColores4 = ( ) => {
 
 const haciaColores5 = () => {
     
-    window.location.href="../pages/seleccioncolores.html";
+    window.location.href="../pages/selcolorniv2.html";
 }
 
 
@@ -21,12 +22,54 @@ const haciaColores5 = () => {
 
 const haciaColores6 = () => {
     
-    window.location.href="../pages/seleccioncolores.html";
+    window.location.href="../pages/selcolorniv3.html";
 }
 
 //-------------------------------------------------------
 
-//FUNCION PARA SELECCIONAR COLOR1 Y ASIGNAR A BOX1
+//FUNCION PARA SELECCIONAR COLOR Y ASIGNAR
+
+//COLORPICKER
+
+// existen dos parametros (inputId y CircleId), 
+// recogen el valor del ID color. 
+//Se declaran entonces las variables (colorInput y circle).
+
+function colorPicker(inputId, circleId, colorsArray) {
+    let colorInput = document.getElementById(inputId);
+    let circle = document.getElementById(circleId);
+
+    colorInput.oninput = () => {
+        circle.style.backgroundColor = colorInput.value;
+        console.log("Color seleccionado:", colorInput.value);
+
+        //Convertir en Array los colores elegidos por el jugador, que luego sirven de comparacion.
+        colorsArray.push(colorInput.value);
+        console.log("Colores almacenados:", colorsArray);
+
+        //Almacenaje colores del jugador en sessionStorage y convertidos en un string.
+        sessionStorage.setItem("almacenColores", JSON.stringify(colorsArray));
+        console.log("Colores almacenados en sessionStorage: ", colorsArray);
+    };
+}
+
+let seleccionColores = [];
+
+//Guardado y converversión del string a array de los colores almacenados en el sessionStorage
+// const storageColores = sessionStorage.getItem("almacenColores");
+//     if (storageColores) {
+//         almacenColores = JSON.parse(storageColores);
+//         console.log("Colores convertidos del sessionStorage:", almacenColores);
+//     }
 
 
+    
+// String y arrays de los colores hexadecimales
+colorPicker("cp1", "circle1", seleccionColores);
+colorPicker("cp2", "circle2", seleccionColores);
+colorPicker("cp3", "circle3", seleccionColores);
+colorPicker("cp4", "circle4", seleccionColores);
+colorPicker("cp5", "circle5", seleccionColores);
+colorPicker("cp6", "circle6", seleccionColores);
 
+//-------------------------------------------------------
