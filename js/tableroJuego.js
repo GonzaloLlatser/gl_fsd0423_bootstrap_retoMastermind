ea399420aec25d9303b9b1442e0cbb6aa7c3d51d
Gonza <gonl@gej.com>
2023-05-31 17:28:24 +0200

    feat: Generar bolas de las filas del tablero dinamico

--- js/tableroJuego.js
+++ js/tableroJuego.js
@@ -14,14 +14,14 @@ const createRows = (nivelValor) => {
       
         console.log ("hola estoy aca");
         tableroContenedor.appendChild(row);  
-        
-        for (let i = 0; i < nivelValor; i++) {
 
-            let row = document.createElement("div");
-            row.classList.add("classRow");
+        for (let i = 0; i < 4; i++) {
+
+            let colors = document.createElement("div");
+            colors.classList.add("classColors");
           
             console.log ("hola estoy aca");
-            tableroContenedor.appendChild(row);  
+            row.appendChild(colors);  
         }
 
 
